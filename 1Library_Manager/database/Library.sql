b3b88f1b9071a7458046b4c0c1abe10622b22b88
huyboom22 <huyboom0202@gmail.com>
2025-03-12 23:37:59 +0700

    huy

--- 1Library_Manager/database/Library.sql
+++ 1Library_Manager/database/Library.sql
@@ -160,3 +160,64 @@ SELECT
     GETDATE(),
     0 -- Mặc định chưa thanh toán
 FROM Fines f;
+
+INSERT INTO Books (title, author, publisher, category, description, image, quantity)
+VALUES
+(N'Fullmetal Alchemist', N'Hiromu Arakawa', N'Shueisha', N'Truyện tranh', N'Hai anh em Edward và Alphonse Elric tìm kiếm cách phục hồi cơ thể sau một thí nghiệm alchemy thất bại.', 'images/book/fullmetal.jpg', 9),
+(N'Bleach', N'Tite Kubo', N'Shueisha', N'Truyện tranh', N'Ichigo Kurosaki, một thanh niên sở hữu sức mạnh của một Shinigami, bảo vệ thế giới khỏi linh hồn tà ác.', 'images/book/bleach.jpg', 11),
+(N'Hunter x Hunter', N'Yoshihiro Togashi', N'Shueisha', N'Truyện tranh', N'Gon Freecss tìm kiếm cha mình và khám phá thế giới Hunter đầy nguy hiểm.', 'images/book/hunterxhunter.jpg', 10),
+(N'Yu Yu Hakusho', N'Yoshihiro Togashi', N'Shueisha', N'Truyện tranh', N'Yusuke Urameshi, một học sinh trung học, phải bảo vệ thế giới con người sau khi hy sinh để cứu một đứa trẻ.', 'images/book/yuyuhakusho.jpg', 14),
+(N'One Punch Man', N'ONE', N'Shogakukan', N'Truyện tranh', N'Saitama, một anh hùng có sức mạnh vô địch, tìm kiếm một đối thủ xứng tầm để chiến đấu.', 'images/book/onepunchman.jpg', 13),
+(N'Fairy Tail', N'Hiro Mashima', N'Shueisha', N'Truyện tranh', N'Mối quan hệ giữa Natsu Dragneel và những người bạn trong Hội Fairy Tail trong cuộc phiêu lưu của họ.', 'images/book/fairytail.jpg', 16),
+(N'My Hero Academia', N'Kohei Horikoshi', N'Shueisha', N'Truyện tranh', N'Izuku Midoriya, một cậu bé không có khả năng siêu nhiên, quyết tâm trở thành anh hùng số một.', 'images/book/myheroacademia.jpg', 9),
+(N'Kaguya-sama: Love is War', N'Aka Akasaka', N'Shuisha', N'Truyện tranh', N'Câu chuyện về cuộc chiến tranh tình yêu giữa Kaguya Shinomiya và Miyuki Shirogane tại hội học sinh.', 'images/book/kaguyasama.jpg', 7),
+(N'Jujutsu Kaisen', N'Gege Akutami', N'Shueisha', N'Truyện tranh', N'Yuji Itadori đối mặt với nguy hiểm khi anh ta nuốt phải một ngón tay của Sukuna, một con quái vật cổ xưa.', 'images/book/jujutsukaisen.jpg', 8),
+(N'Demon Slayer', N'Koyoharu Gotouge', N'Shueisha', N'Truyện tranh', N'Tanjiro Kamado chiến đấu chống lại quái vật để cứu em gái mình và trả thù cho gia đình.', 'images/book/demonslayer.jpg', 14),
+(N'Tokyo Ghoul', N'Sui Ishida', N'Shuisha', N'Truyện tranh', N'Kaneeki, một con người trở thành Ghoul, phải đối mặt với những thử thách trong thế giới ghoul đầy tàn nhẫn.', 'images/book/tokyoghoul.jpg', 6),
+(N'Slam Dunk', N'Takehiko Inoue', N'Shueisha', N'Truyện tranh', N'Hanamichi Sakuragi, một thanh niên nóng tính, gia nhập đội bóng rổ để tìm kiếm sự tôn trọng và tình yêu.', 'images/book/slamdunk.jpg', 15),
+(N'Rurouni Kenshin', N'Nobuhiro Watsuki', N'Shueisha', N'Truyện tranh', N'Himura Kenshin, một samurai bị nguyền rủa, chiến đấu để bảo vệ những người vô tội trong thời kỳ Minh Trị.', 'images/book/rurounikenshin.jpg', 8)
+
+-- Insert Japanese manga
+INSERT INTO Books (title, author, publisher, category, description, image, quantity)
+VALUES
+(N'Spirit Circle', N'Daisuke Ashihara', N'Shueisha', N'Truyện tranh', N'Câu chuyện về một cậu bé có thể sống lại trong các cuộc sống khác nhau qua các kiếp trước, phải đối mặt với những thử thách lớn.', 'images/book/spiritcircle.jpg', 8),
+(N'Platinum End', N'Tsugumi Ohba', N'Verve', N'Truyện tranh', N'Một câu chuyện về một cậu bé bị bỏ rơi phải chiến đấu để trở thành một thiên thần, khi một cuộc chiến tranh giành quyền lực với những thiên thần khác bắt đầu.', 'images/book/platinumend.jpg', 9);
+
+
+INSERT INTO books (title, author, publisher, category, description, image, quantity)
+VALUES
+(N'Kinh Tế Học', N'Paul A. Samuelson', N'Nhà xuất bản Đại học Kinh tế Quốc dân', N'Khởi nghiệp', N'Giới thiệu các lý thuyết cơ bản trong kinh tế học.', 'images/book/kinhtehoc.jpg', 9),
+(N'Start with Why', N'Simon Sinek', N'Nhà xuất bản Trẻ', N'Khởi nghiệp', N'Khám phá lý do đằng sau thành công của các tổ chức.', 'images/book/startwithwhy.jpg', 10),
+(N'Chiến Lược Đại Dương Xanh', N'W. Chan Kim & Renée Mauborgne', N'Nhà xuất bản Trẻ', N'Khởi nghiệp', N'Một cách tiếp cận chiến lược để mở rộng thị trường mà không có đối thủ cạnh tranh.', 'images/book/chienluocdaiduongxanh.jpg', 9),
+(N'Billion Dollar Whale', N'Tom Wright & Bradley Hope', N'Nhà xuất bản Tổng hợp TP.HCM', N'Khởi nghiệp', N'Câu chuyện về vụ lừa đảo tài chính lớn nhất trong lịch sử hiện đại.', 'images/book/billiondollarwhale.jpg', 8),
+(N'Good to Great', N'Jim Collins', N'Nhà xuất bản Tổng hợp TP.HCM', N'Khởi nghiệp', N'Nghiên cứu lý do tại sao một số công ty có thể duy trì sự xuất sắc lâu dài.', 'images/book/goodtogreat.jpg', 10),
+(N'Grit', N'Angela Duckworth', N'Nhà xuất bản Trẻ', N'Khởi nghiệp', N'Mạnh mẽ và kiên cường là yếu tố quan trọng trong việc đạt được thành công.', 'images/book/grit.jpg', 9),
+(N'Lean In', N'Sheryl Sandberg', N'Nhà xuất bản NXB Phụ nữ', N'Khởi nghiệp', N'Một thông điệp mạnh mẽ về sự tham gia và lãnh đạo của phụ nữ trong xã hội.', 'images/book/leanin.jpg', 8),
+(N'Rich Dad Poor Dad', N'Robert Kiyosaki', N'Nhà xuất bản Trẻ', N'Khởi nghiệp', N'Giới thiệu về sự khác biệt trong tư duy và đầu tư giữa người giàu và người nghèo.', 'images/book/richdadpoordad.jpg', 10),
+(N'Người Giàu Nhất Thành Babylon', N'George S. Clason', N'Nhà xuất bản Tổng hợp TP.HCM', N'Khởi nghiệp', N'Một cuốn sách về các nguyên tắc tài chính cơ bản, giúp bạn quản lý tiền bạc hiệu quả và đầu tư thông minh.', 'images/book/nguoigiauchobabylon.jpg', 8),
+(N'Mindset', N'Carol S. Dweck', N'Nhà xuất bản Trẻ', N'Khởi nghiệp', N'Sự khác biệt giữa tư duy cố định và tư duy phát triển sẽ ảnh hưởng thế nào đến thành công của bạn.', 'images/book/mindset.jpg', 9);
+
+INSERT INTO books (title, author, publisher, category, description, image, quantity)
+VALUES
+(N'Đời thay đổi khi chúng ta thay đổi', N'Andrew Matthews', N'Nhà xuất bản Trẻ', N'Tâm linh', N'Khám phá cách thay đổi tư duy và cuộc sống của bạn thông qua những bài học từ cuộc sống.', 'images/book/doithaydoikhichungtathaydoi.jpg', 9),
+(N'Chánh niệm trong từng hơi thở', N'Tich Nhat Hanh', N'Nhà xuất bản Thế Giới', N'Tâm linh', N'Hướng dẫn về chánh niệm và những lợi ích của việc sống trong khoảnh khắc hiện tại.', 'images/book/chanhniemtrongtunghoitho.jpg', 9),
+(N'Để sống hạnh phúc', N'Anthony Robbins', N'Nhà xuất bản NXB Phụ nữ', N'Tâm linh', N'Khám phá cách thức sống hạnh phúc và đạt được sự bình an trong tâm hồn.', 'images/book/desonghanhphuc.jpg', 9),
+(N'Bí mật của sức mạnh tiềm thức', N'Joseph Murphy', N'Nhà xuất bản Trẻ', N'Tâm linh', N'Khám phá sức mạnh của tiềm thức và cách nó ảnh hưởng đến cuộc sống của bạn.', 'images/book/bimatcuasucmanhtiembut.jpg', 10);
+
+
+INSERT INTO books (title, author, publisher, category, description, image, quantity)
+VALUES
+(N'Khả Năng Tư Duy Đột Phá', N'Edward de Bono', N'Nhà xuất bản Trẻ', N'Tư duy', N'Khám phá cách phát triển tư duy sáng tạo và đột phá.', 'images/book/khanangtuduydotpha.jpg', 9),
+(N'Tư Duy Tích Cực', N'Norman Vincent Peale', N'Nhà xuất bản Thế Giới', N'Tư duy', N'Khám phá sức mạnh của tư duy tích cực trong cuộc sống và công việc.', 'images/book/tuduytichcuc.jpg', 10),
+(N'Chìa Khóa Thành Công', N'Brian Tracy', N'Nhà xuất bản Tổng hợp TP.HCM', N'Tư duy', N'Những nguyên tắc tư duy và hành động giúp đạt được thành công bền vững.', 'images/book/chiakhoathanhcong.jpg', 8),
+(N'Giới Hạn Của Bạn Là Gì?', N'Gareth Jones', N'Nhà xuất bản Thế Giới', N'Tư duy', N'Khám phá giới hạn của bản thân và cách vượt qua chúng để đạt được mục tiêu.', 'images/book/gioihanbatlaigi.jpg', 8)
+
+INSERT INTO books (title, author, publisher, category, description, image, quantity)
+VALUES
+(N'Giết Con Chim Nhại', N'Harper Lee', N'Nhà xuất bản Văn học', N'Tiểu thuyết', N'Câu chuyện về sự phân biệt chủng tộc và công lý tại miền Nam nước Mỹ.', 'images/book/gietconchimnhai.jpg', 19),
+(N'To Kill a Mockingbird', N'Harper Lee', N'Nhà xuất bản Tổng hợp TP.HCM', N'Tiểu thuyết', N'Một câu chuyện về tình yêu và sự công bằng trong xã hội phân biệt chủng tộc.', 'images/book/tokillamockingbird.jpg', 20),
+(N'1984', N'George Orwell', N'Nhà xuất bản Văn hóa - Văn nghệ', N'Tiểu thuyết', N'Câu chuyện về một xã hội toàn trị nơi chính phủ kiểm soát mọi thứ, bao gồm cả suy nghĩ của con người.', 'images/book/1984.jpg', 18),
+(N'Mắt Biếc', N'Nguyễn Nhật Ánh', N'Nhà xuất bản Trẻ', N'Tiểu thuyết', N'Cuốn sách kể về tình yêu đầu đời trong sáng và những ký ức đẹp của tuổi thơ.', 'images/book/matbiec.jpg', 17),
+(N'Nhà Giả Kim', N'Paulo Coelho', N'Nhà xuất bản Thế Giới', N'Tiểu thuyết', N'Cuốn sách kể về hành trình tìm kiếm ý nghĩa cuộc sống và sự giàu có đích thực.', 'images/book/nhagiakim.jpg', 20),
+(N'Cánh Đồng Bất Tận', N'Nguyễn Ngọc Tư', N'Nhà xuất bản Trẻ', N'Tiểu thuyết', N'Một tác phẩm về cuộc sống và những khó khăn của người dân vùng quê miền Tây Nam Bộ.', 'images/book/canhdombutan.jpg', 18);
+
